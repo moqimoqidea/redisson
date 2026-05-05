6c7bddc89e1ca137db25f51e16eb22348dad63ae
mrniko <1104661+mrniko@users.noreply.github.com>
2026-05-05 08:56:50 +0300

    old tests removed

--- redisson/src/test/java/org/redisson/BaseTest.java
+++ redisson/src/test/java/org/redisson/BaseTest.java
@@ -1,12 +1,6 @@
 package org.redisson;
 
-import java.io.IOException;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 
 @Deprecated
 public abstract class BaseTest {
